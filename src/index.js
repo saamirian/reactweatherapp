6dc3759dfbe3d175c14d4d1dcbe1a358cbc1cdf5
saharamirian <45035127+saharamirian@users.noreply.github.com>
2019-06-13 10:51:14 +1000

    index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDom from "react-dom"
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from "./store"
+import App from "./App"
+import './styles/base.scss'
+
+
+const appp = 
+<Provider store={store}>
+<BrowserRouter >
+<App />
+</BrowserRouter>
+</Provider>
+
+ReactDom.render (
+appp,document.getElementById('reactroot')
+)
